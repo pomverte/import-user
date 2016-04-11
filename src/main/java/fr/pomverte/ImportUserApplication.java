165d4e577af9375d42f842bf823703ecc0959bfa
pomverte <vietnem@yahoo.fr>
2016-04-11 13:36:52 +0200

    JobRepository configuration in memory

--- src/main/java/fr/pomverte/ImportUserApplication.java
+++ src/main/java/fr/pomverte/ImportUserApplication.java
@@ -2,8 +2,9 @@ package fr.pomverte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ImportUserApplication {
 
 	public static void main(String[] args) {
